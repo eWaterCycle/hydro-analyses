242e036ff297ab5de41b7c3ca8d5270c5cac409e
sebranchett <s.e.branchett@tudelft.nl>
2019-03-26 09:45:49 +0100

    linting improvements

--- grdc_explore/grdc_explore.py
+++ grdc_explore/grdc_explore.py
@@ -3,34 +3,44 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 
+
 def dataframe_from_stationId(grdc_folder, station_id):
-    ''' Search the grdc_folder for a file with name <station_id>.day,
+    '''Create dataframe from grdc file.
+
+    Search the grdc_folder for a file with name <station_id>.day,
     skip the header rows and return the dates and discharges in a dataframe'''
-                
+
     filename = str(station_id) + ".day"
     grdc_file = os.path.join(grdc_folder, filename)
 
     grdc_data = pd.read_csv(grdc_file, skiprows=40, delimiter=';')
     grdc_data = grdc_data.rename(columns={'YYYY-MM-DD': 'date',
                                  ' Original': 'discharge'})
-    grdc_data = grdc_data.reset_index().set_index(pd.DatetimeIndex(grdc_data['date']))
+    grdc_data = grdc_data.reset_index().set_index(pd.DatetimeIndex(
+                                                  grdc_data['date']))
     grdc_data = grdc_data[['date', 'discharge']]
- 
+
     return grdc_data
 
-def valid_series(cleaned, num_invalid_days=0):
-    ''' Search through a cleaned and sorted dataframe and return a dataframe with start
-    end dates of series with (nearly) consecutive days of valid discharge data.
-    num_invalid_days is the number of consecutive days in a series that may contain invalid data.
-    
-    Every row in the cleaned, input dataframe must have valid data points for all stations. '''
 
-    series = pd.DataFrame(columns=['Start Date', 'End Date', 'Number of days in series'])
+def valid_series(cleaned, num_invalid_days=0):
+    ''' Idendify valid series of data for all stations.
+
+    Search through a cleaned and sorted dataframe and return a
+    dataframe with start end dates of series with (nearly) consecutive
+    days of valid discharge data. num_invalid_days is the number of
+    consecutive days in a series that may contain invalid data.
+
+    Every row in the cleaned, input dataframe must have valid data points
+    for all stations. '''
+
+    series = pd.DataFrame(columns=['Start Date', 'End Date',
+             'Number of days in series'])
 
     init = True
     for index, row in cleaned.iterrows():
         date = datetime.strptime(str(index), "%Y-%m-%d %H:%M:%S")
-        if init: # initialise the first time around
+        if init:  # initialise the first time around
             start_date = date
             end_date = date
             init = False
@@ -41,43 +51,58 @@ def valid_series(cleaned, num_invalid_days=0):
             series_length = (end_date - start_date).days
             if (series_length > 0):
                 series = series.append({'Start Date': start_date,
-                          'End Date': end_date,
-                          'Number of days in series': series_length+1}, ignore_index=True)
+                         'End Date': end_date,
+                         'Number of days in series': series_length+1},
+                         ignore_index=True)
             start_date = date
             end_date = start_date
-            
+
     # end the final series
     series_length = (end_date - start_date).days
-    if (series_length > 0): # 
-        series = series.append({'Start Date': start_date, 'End Date': end_date,
-                                'Number of days in series': series_length+1}, ignore_index=True)
+    if (series_length > 0):
+        series = series.append({'Start Date': start_date,
+                                'End Date': end_date,
+                                'Number of days in series': series_length+1},
+                                ignore_index=True)
     return series
 
+
 def fill_with_empty_rows(original, first_day, last_day):
-    ''' take a dataframe with a sorted Timestamp index (one row per day)
+    '''Complete a dataframe with a row for each day in a range.
+
+    Take a dataframe with a sorted Timestamp index (one row per day)
     and add missing days '''
+
     frame = original.copy()
-    for day in (first_day + pd.to_timedelta(n, unit='d') for n in range((last_day-first_day).days + 1)):
+    for day in (first_day + pd.to_timedelta(n, unit='d')
+                for n in range((last_day-first_day).days + 1)):
         if day not in frame.index:
             frame.loc[day] = pd.Series([np.nan])
             frame = frame.sort_index()
     return(frame)
 
+
 def fill_nans(value_before, value_after, num_vals):
-    ''' Make a list of values, num_vals long, that are a linear interpolation between
-    value_before and value_after. '''
-    
+    '''Generate linearly interpolated values over a range.
+
+    Make a list of values, num_vals long, that are a linear interpolation
+    between value_before and value_after. '''
+
     # todo check if num_vals is an integer
     vals = []
     delta = (value_after - value_before) / (num_vals + 1)
-    for i in range (num_vals):
+    for i in range(num_vals):
         vals.append(value_before + ((i + 1) * delta))
     return vals
 
+
 def replace_nans(original):
-    ''' Return a copy of a dataframe, with the NaNs replaced by linearly interpolated values.
+    '''Replace NaNs with linearly interpolated values.
+
+    Return a copy of a dataframe, with the NaNs replaced by linearly
+    interpolated values.
     The values are interpolated for each column/station in turn. '''
-    
+
     interpolated = original.copy()
     for column in original:
         # for each station in turn, look for the invalid discharge values
@@ -91,16 +116,18 @@ def replace_nans(original):
                 low_val = value
                 init = False
             elif (interp_zone):
-                # in a NaN zone,
-                # looking for the next valid value so it is possible to interpolate
+                # in a NaN zone, looking for the next valid value so
+                # it is possible to interpolate
                 if (np.isnan(value)):
-                    pass # already in a NaN zone before we got here
+                    pass  # already in a NaN zone before we got here
                 else:
                     high_idx = index
                     high_val = value
-                    fill_values = (fill_nans(low_val, high_val, (high_idx - low_idx).days - 1))
+                    fill_values = (fill_nans(low_val, high_val,
+                                      (high_idx - low_idx).days - 1))
                     interpolated.loc[(low_idx + pd.Timedelta(days=1)):
-                                     (high_idx - pd.Timedelta(days=1)), column] = fill_values
+                                     (high_idx - pd.Timedelta(days=1)),
+                                     column] = fill_values
                     low_idx = index
                     low_val = value
                     interp_zone = False
--- grdc_explore/test/test_grdc_explore.py
+++ grdc_explore/test/test_grdc_explore.py
@@ -6,60 +6,71 @@ import numpy as np
 import pandas as pd
 from pandas.io.parsers import EmptyDataError
 
-from grdc_explore import dataframe_from_stationId, valid_series, fill_with_empty_rows, fill_nans, replace_nans
+from grdc_explore import dataframe_from_stationId, valid_series, \
+     fill_with_empty_rows, fill_nans, replace_nans
 
 grdc_data = os.path.join(os.path.dirname(__file__), "grdc_data")
+
 
 def test_dataframe_from_stationId():
     df = dataframe_from_stationId(grdc_data, '30_datalines')
     assert len(df) == 30
 
+
 def test_dataframe_file_too_short():
     with pytest.raises(EmptyDataError):
-        df = dataframe_from_stationId(grdc_data, 'too_short')
+        dataframe_from_stationId(grdc_data, 'too_short')
+
 
 def test_dataframe_wrong_column_name():
     with pytest.raises(KeyError):
-        df = dataframe_from_stationId(grdc_data, 'wrong_column')
+        dataframe_from_stationId(grdc_data, 'wrong_column')
+
 
 def test_dataframe_missing_file():
     with pytest.raises(Exception):
-        df = dataframe_from_stationId(grdc_data, 'missing_file')
+        dataframe_from_stationId(grdc_data, 'missing_file')
+
 
 # read in 2 grdc datafiles and combine the data for hte following tests
 df_1 = dataframe_from_stationId(grdc_data, 'series_1')
 df_2 = dataframe_from_stationId(grdc_data, 'series_2')
 combined = df_1.drop(columns=['date']).\
-        rename(columns={'discharge': 'discharge_1'})
+                     rename(columns={'discharge': 'discharge_1'})
 combined = pd.merge(combined, df_2.drop(columns=['date']),
-        how='outer', on='date', sort=True)
+                    how='outer', on='date', sort=True)
 combined = combined.rename(columns={'discharge': 'discharge_2'})
 combined = combined.replace(-999., np.nan)
 
 cleaned = combined.dropna(axis=0, how='any')
-    
+
+
 def test_valid_series():
     num_invalid_days = 0
     series = valid_series(cleaned, num_invalid_days)
     results = [10, 2, 3, 4]
     assert all([a == b for a, b in zip(results,
-        series['Number of days in series'].values)])
-    
+               series['Number of days in series'].values)])
+
+
 def test_valid_1_invalid():
     num_invalid_days = 2
     series = valid_series(cleaned, num_invalid_days)
     results = [28]
     print(series['Number of days in series'].values)
     assert all([a == b for a, b in zip(results,
-        series['Number of days in series'].values)])
+               series['Number of days in series'].values)])
+
 
 def test_fill_with_empty_rows():
     input_df = df_1.drop(columns=['date'])
     input_df = input_df.replace(-999., np.nan)
     input_df = input_df.dropna(axis=0, how='any')
-    output_df = fill_with_empty_rows(input_df, input_df.index[0], input_df.index[-1])
+    output_df = fill_with_empty_rows(input_df, input_df.index[0],
+                                     input_df.index[-1])
     assert len(input_df) == 24
     assert len(output_df) == 28
+
 
 def test_fill_nans():
     before = 1.1
@@ -68,6 +79,7 @@ def test_fill_nans():
     results = [2.2, 3.3, 4.4]
     filling = fill_nans(before, after, posts)
     assert all([a == pytest.approx(b) for a, b in zip(results, filling)])
+
 
 def test_replace_nans():
     df = pd.DataFrame([[1.,      1.],
@@ -84,6 +96,6 @@ def test_replace_nans():
     filled = replace_nans(df)
     results = [1., 2., 3., 4., 5., 6., 7.]
     assert all([a == b for a, b in zip(results,
-        filled['miss one'].values)])
+               filled['miss one'].values)])
     assert all([a == b for a, b in zip(results,
-        filled['miss three'].values)])
+               filled['miss three'].values)])
